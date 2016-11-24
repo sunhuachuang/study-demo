4de674dc8b81c1bce7e09e0bb65e4037c472bef4
sun <huachuang20@gmail.com>
2016-11-24 14:36:28 +0800

    over 棋盘问题，汉诺塔问题

--- algorithm/枚举/小于n的最大素数/sieve_of_Eratosthenes.py
+++ algorithm/枚举/小于n的最大素数/sieve_of_Eratosthenes.py
@@ -3,16 +3,17 @@ import math;
 
 n = 100000;
 
-a = [True for i in range(0, n)]
+a = {i:True for i in range(1, n+1)}
 
 for x in range(2, int(math.sqrt(n))+1):
     for y in range(2, int(n/x)+1):
-        if x*y-1 <= n:
-            a[y*x-1] = False;
+        if x*y <= n:
+            a[y*x] = False;
 
 last = 2;
-for key, value in enumerate(a):
+
+for key, value in a.items():
     if value:
-        last = key + 1;
+        last = key;
 
 print(last); #0.28s user 0.01s system 98% cpu 0.292 total
