960a8ba107644ffbd62f4c4598e09b779dcc6482
sun <huachuang20@gmail.com>
2017-07-03 02:40:23 +0000

    大部分语言都可以直接使用异或操作符

--- c_c++/replace_two_variable.c
+++ c_c++/replace_two_variable.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  int x[] = {1, 2};
-  int y[] = {3, 4};
+  int m = 1;
+  int n = 2;
 
-  *y = *x ^ *y; //数组指针指向第一个元素
-  *x = *x ^ *y;
-  *y = *x ^ *y;
+  n = m ^ n;
+  m = m ^ n;
+  n = m ^ n;
 
-  for (int i = 0; i < 2; i++ ) {
-    printf("x[%d] = %d\n", i, x[i]); // [3, 2]
-  }
-
-  for (int i = 0; i < 2; i++ ) {
-    printf("y[%d] = %d\n", i, y[i]); // [1, 4]
-  }
+  printf("m is %d, n is %d\n", m, n); // m is 2, n is 1
 
   int a = 1;
   int b = 2;
@@ -29,6 +23,21 @@ int main() {
   *bp = *ap ^ *bp;
 
   printf("a is %d, b is %d\n", a, b); // a is 2, b is 1
+
+  int x[] = {1, 2};
+  int y[] = {3, 4};
+
+  *y = *x ^ *y; //数组指针指向第一个元素
+  *x = *x ^ *y;
+  *y = *x ^ *y;
+
+  for (int i = 0; i < 2; i++ ) {
+    printf("x[%d] = %d\n", i, x[i]); // [3, 2]
+  }
+
+  for (int i = 0; i < 2; i++ ) {
+    printf("y[%d] = %d\n", i, y[i]); // [1, 4]
+  }
 
   return 0;
 }
